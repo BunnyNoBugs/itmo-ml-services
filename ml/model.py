2d3baca614f6fb0c55f7ab18303b1397e442dd20
BunnyNoBugs <eugene200020@gmail.com>
2024-01-23 02:51:01 +0300

    Update model.py

--- ml/model.py
+++ ml/model.py
@@ -7,9 +7,4 @@ def load_model(model_type: str = 'lr'):
     with open(f'models/{model_type}_model.pickle', 'rb') as f:
         model_instance = pickle.load(f)
 
-    def model(input_df: pd.DataFrame):
-        pred = model_instance.predict(input_df)
-
-        return pred
-
     return model_instance
